84027d639fc728d2ad3fb9fdb716d1d9ca13348a
Shivam Singh Gangola <152065144+CrshIVam16@users.noreply.github.com>
2026-08-29 10:45:03 +0530

    deleted one unnecessary screenshot

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,17 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    {function Counter() {
+      const [count, setCount] = useState(0);
+
+      const handleClick = () => {
+        setCount(count + 1);
+        setCount(count + 1);
+        setCount(count + 1);
+      };
+
+      return <button onClick={handleClick}>Count: {count}</button>;
+    }}
   </StrictMode>,
 )
